efd96937f01fbf372dad52502c34b5b95cb3dd5c
ogneyar <ya13th@mail.ru>
2023-05-12 15:11:41 +0300

    auto commit

--- server/service/mailService.js
+++ server/service/mailService.js
@@ -12,7 +12,11 @@ class MailService {
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
-            }
+            },
+            tls: {
+              // do not fail on invalid certs
+              rejectUnauthorized: false,
+            },
         })
     }
 
